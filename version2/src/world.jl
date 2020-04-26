11564e25039a59773c86c964e5cee8c0987948bf
George Matheos <georgematheos@gmail.com>
2020-04-26 14:52:09 -0400

    throw error if not all generate constraints used

--- version2/src/world.jl
+++ version2/src/world.jl
@@ -125,23 +125,62 @@ end
 @inline get_gen_fn(world::World, addr::Symbol) = get_gen_fn(world, Val(addr))
 @inline get_gen_fn(world::World, ::Call{addr}) where {addr} = get_gen_fn(world, addr)
 
-has_value(world::World, call::Call) = haskey(world.subtraces, call)
+has_value_for_call(world::World, call::Call) = haskey(world.subtraces, call)
 total_score(world::World) = world.total_score
 
 ##############
 # Generation #
 ##############
 
+"""
+    get_ungenerated_constraints(world, constraints)
+    
+Returns a list of all the Calls which constraints were provided for, but which
+do not have a value generated for them in the world.
+"""
+function get_ungenerated_constraints(world::World, constraints::ChoiceMap)
+    unused_constraints = []
+    for (address, key_to_submap) in get_submaps_shallow(constraints)
+        for (key, submap) in get_submaps_shallow(key_to_submap)
+            c = Call(address, key)
+            if !has_value_for_call(world, c)
+                push!(unused_constraints, c)
+            end
+        end
+    end
+    return unused_constraints
+end
+
+"""
+    begin_generate!(world, constraints)
+
+Tell the world we are about to run generate on the kernel.
+`get_submap(constraints, addr => key)` contains the constraints
+which should be used to generate the value for call `(addr, key)`.
+All calls provided in the call must have a value generated for them
+before generation finishes (as denoted by calling `end_generate!`).
+"""
 function begin_generate!(world::World, constraints::ChoiceMap)
     @assert (world.state isa NoChangeWorldState) "World is currently in a $(typeof(world.state)); cannot begin a generate."
     world.state = GenerateWorldState(constraints)
 end
 
-function end_generate!(world::World)
+"""
+    end_generate!
+Tell the world that we are done running generate. If `check_all_constraints_used`
+is true (as it is by default), but some of the constraints provided in `begin_generate!`
+were for a call which never had a value generated for it, an error will be thrown.
+"""
+function end_generate!(world::World; check_all_constraints_used=true)
     @assert world.state isa GenerateWorldState
     
-    # TODO: check that all constraints were used
-    
+    if check_all_constraints_used
+        unused_constraints = get_ungenerated_constraints(world, world.state.constraints)
+        if length(unused_constraints) != 0
+            throw(error("The following constraints were not used while generating: $unused_constraints !"))
+        end
+    end
+        
     # save the topological sort for the calls in the world
     world.call_sort = PersistentHashMap(([call => i for (i, call) in enumerate(world.state.call_sort)]::Vector{<:Pair{<:Call, Int}})...)
     weight = world.state.weight
@@ -154,7 +193,7 @@ end
 function lookup_or_generate!(world::World, call::Call)
     @assert world.state isa GenerateWorldState
     
-    if !has_value(world, call)
+    if !has_value_for_call(world, call)
         generate_value!(world, call)
     end
 
@@ -165,7 +204,7 @@ end
 
 function generate_value!(world, call)
     @assert world.state isa GenerateWorldState
-    @assert !has_value(world, call)
+    @assert !has_value_for_call(world, call)
     
     gen_fn = get_gen_fn(world, call)
     constraints = get_submap(world.state.constraints, addr(call) => key(call))
