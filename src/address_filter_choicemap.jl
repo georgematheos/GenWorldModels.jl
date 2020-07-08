e1ac1b1b21441c59a21b00e6b7b301f72e7731e4
George Matheos <georgematheos@gmail.com>
2020-07-08 12:09:30 -0400

    update address filter choicemap to new Gen version

--- src/address_filter_choicemap.jl
+++ src/address_filter_choicemap.jl
@@ -7,66 +7,27 @@ Wraps `choicemap` and filters out branches with a top-level address such that
 Ie. this exposes all choices in `choicemap`, unless the address `node1 => node2 => ... => node_n`
 is such that `f(node_m)` is false, in which case this choice will not appear in the choicemap.
 """
-struct AddressFilterChoicemap <: Gen.ChoiceMap
-    choicemap::ChoiceMap
+struct AddressFilterAddressTree{LeafType} <: Gen.AddressTree{LeafType}
+    tree::Gen.AddressTree{LeafType}
     f::Function
 end
+const AddressFilterChoiceMap = AddressFilterAddressTree{Gen.Value}
+AddressFilterAddressTree(t::Gen.AddressTreeLeaf, f::Function) = t
+AddressFilterAddressTree{LT}(t::Gen.AddressTreeLeaf, f::Function) where {LT} = t
 
-Gen.has_value(c::AddressFilterChoicemap, addr) = c.f(addr) && has_value(c.choicemap, addr)
-function Gen.has_value(c::AddressFilterChoicemap, addr::Pair)
-    (first, rest) = addr
-    has_value(get_submap(c, first), rest)
-end
-
-function Gen.get_submap(c::AddressFilterChoicemap, addr)
-    if !c.f(addr)
-        EmptyChoiceMap()
+function Gen.get_subtree(t::AddressFilterAddressTree, addr)
+    if !t.f(addr)
+        EmptyAddressTree()
     else
-        AddressFilterChoicemap(get_submap(c.choicemap, addr), c.f)
+        AddressFilterAddressTree(get_subtree(t.tree, addr), t.f)
     end
 end
-function Gen.get_submap(c::AddressFilterChoicemap, addr::Pair)
-    first, rest = addr
-    if !c.f(first)
-        EmptyChoiceMap()
-    else
-        submap = get_submap(c, first)
-        get_submap(submap, rest)
-    end
-end
+Gen.get_subtree(t::AddressFilterAddressTree, addr::Pair) = Gen._get_subtree(t, addr)
 
-function Gen.get_value(c::AddressFilterChoicemap, addr)
-    if !c.f(addr)
-        throw(KeyError(addr))
-    else
-        get_value(c.choicemap, addr)
-    end
-end
-function Gen.get_value(c::AddressFilterChoicemap, addr::Pair)
-    first, rest = addr
-    if !c.f(first)
-        throw(KeyError(first))
-    else
-        get_value(get_submap(c, first), rest)
-    end
-end
-
-function Gen.get_values_shallow(c::AddressFilterChoicemap)
+function Gen.get_subtrees_shallow(t::AddressFilterAddressTree)
     (
-        addr_val
-        for addr_val in get_values_shallow(c.choicemap)
-        if c.f(addr_val[1])
+        (addr, AddressFilterAddressTree(subtree, t.f))
+        for (addr, subtree) in get_subtrees_shallow(t.tree)
+        if t.f(addr)
     )
-end
-
-function Gen.get_submaps_shallow(c::AddressFilterChoicemap)
-    (
-        (addr, AddressFilterChoicemap(submap, c.f))
-        for (addr, submap) in get_submaps_shallow(c.choicemap)
-        if c.f(addr)
-    )
-end
-
-function Base.isempty(c::AddressFilterChoicemap)
-    isempty(get_values_shallow(c)) && all(isempty(submap) for (_, submap) in (get_submaps_shallow(c)))
 end
