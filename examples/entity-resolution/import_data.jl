17a974d273dd0661d04ece0e445dc281648acce7
George Matheos <georgematheos@gmail.com>
2020-09-16 09:24:42 -0400

    work on realworld data run

--- examples/entity-resolution/import_data.jl
+++ examples/entity-resolution/import_data.jl
@@ -1,42 +1,58 @@
+struct SentenceNumeric
+    verb::Int
+    ent1::Int
+    ent2::Int
+end
+struct FactNumeric
+    rel::Int
+    ent1::Int
+    ent2::Int
+end
 
-function read_fact_data()
-    fact_data = JSON.parsefile(joinpath(@__DIR__, "testdata.json"))
-    fact_data = fact_data["facts"]
-    idx = 1
-    sentences = Tuple{Int, Int, Int}[]
-    entities = Dict()
-    verbs = Dict()
-    ent_idx = 1
-    verb_idx = 1
-    fact_idx=1
-    fact_idx_to_sentence_indices = []
-    sentence_idx_to_fact = []
-    for (sentences_for_fact) in fact_data
-        sentence_indices_for_fact = []
-        for sentence in sentences_for_fact
-            ent1 = sentence["source"]
-            ent2 = sentence["dest"]
-            verb = sentence["depPath"]
-            if !haskey(entities, ent1)
-                entities[ent1] = ent_idx
-                ent_idx += 1
-            end
-            if !haskey(entities, ent2)
-                entities[ent2] = ent_idx
-                ent_idx += 1
-            end
-            if !haskey(verbs, verb)
-                verbs[verb] = verb_idx
-                verb_idx += 1
-            end
-            push!(sentences, (entities[ent1], verbs[verb], entities[ent2]))
-            push!(sentence_idx_to_fact, fact_idx)
-            push!(sentence_indices_for_fact, idx)
-            idx += 1
-        end
-        fact_idx += 1
-        push!(fact_idx_to_sentence_indices, sentence_indices_for_fact)
+struct LabeledDataSet
+    sentences_numeric::Vector{SentenceNumeric}
+    facts_numeric::Vector{FactNumeric}
+    entity_strings::Vector{String}
+    verb_strings::Vector{String}
+    relation_strings::Vector{String}
+end
+
+read_data() = read_data("testdata.json")
+function read_data(filename)
+    data = JSON.parsefile(joinpath(@__DIR__, filename))
+
+    sentences_numeric = []
+    facts_numeric = []
+    entity_strings = []
+    verb_strings = []
+    relation_strings = []
+
+    verb_idx = Dict{String, Int}()
+    ent_idx = Dict{String, Int}()
+    get_verb_idx!(verb) = haskey(verb_idx, verb) ? verb_idx[verb] : begin
+        push!(verb_strings, verb)
+        verb_idx[verb] = length(verb_strings)
+        length(verb_strings)
+    end
+    get_ent_idx!(ent) = haskey(ent_idx, ent) ? ent_idx[ent] : begin
+        push!(entity_strings, ent)
+        ent_idx[ent] = length(entity_strings)
+        length(entity_strings)
     end
 
-    return (sentences, collect(enumerate(fact_idx_to_sentence_indices)), sentence_idx_to_fact, length(entities), length(verbs))
+    for rel_datum in data["relData"]
+        push!(relation_strings, rel_datum["relName"])
+        rel_idx = length(relation_strings)
+        for fact_datum in rel_datum["facts"]
+            for sentence in fact_datum
+                e1 = get_ent_idx!(sentence["source"])
+                e2 = get_ent_idx!(sentence["dest"])
+                verb = get_verb_idx!(sentence["depPath"])
+                push!(facts_numeric, FactNumeric(rel_idx, e1, e2))
+                push!(sentences_numeric, SentenceNumeric(verb, e1, e2))
+            end
+        end
+    end
+
+    return LabeledDataSet(sentences_numeric, facts_numeric, entity_strings, verb_strings, relation_strings)
 end
