820c03a4659c73d5f51039cd8a7444e49b705433
George Matheos <georgematheos@gmail.com>
2020-10-10 12:02:43 -0400

    ie: synthetic data entropy analysis

--- examples/entity-resolution/analysis/analyze.jl
+++ examples/entity-resolution/analysis/analyze.jl
@@ -2,6 +2,7 @@ module DataAnalysis
 using JSON
 using Serialization
 using Plots
+using Dates
 include("../types.jl")
 include("entropy_metric.jl")
 include("../data/import_data.jl")
@@ -13,38 +14,40 @@ function get_state(filename)
     st
 end
 
-function calculate_entropies(datadir)
-    data = read_data()
-    nrels = length(data.relation_strings)
-    sentence_rels = map(fact -> fact.rel, data.facts_numeric)
-    ground_truth = State(nrels, Set{FactNumeric}(), sentence_rels)    
+function calculate_entropies_over_time(datadir)
+    filenames = [name for name in readdir(datadir, sort=true)]
+    ground_truth = get_state(joinpath(datadir, "groundtruth"))
 
-    filenames = [name for name in readdir(datadir, sort=true) if tryparse(Int, name) !== nothing]
+    times = []
     entropies_of_groundtruth_given_inferred = []
     entropies_of_inferred_given_groundtruth = []
-    indices = []
     for filename in filenames
+        if filename == "groundtruth"
+            continue
+        end
         state = get_state(joinpath(datadir, filename))
 
         push!(entropies_of_groundtruth_given_inferred, entropy_of_second_given_first(state, ground_truth))
         push!(entropies_of_inferred_given_groundtruth, entropy_of_second_given_first(ground_truth, state))
-        push!(indices, parse(Int, filename))
-        #gui()
-    end
 
-    return (entropies_of_groundtruth_given_inferred, entropies_of_inferred_given_groundtruth, indices)
+        datetime = DateTime(split(filename, "---")[2], "yyyymmdd-HH_MM_SS-sss")
+        push!(times, datetime)
+    end
+    mintime = min(times...)
+    runtimes = [Dates.value(time - mintime)/1000 for time in times] # times in seconds since first recorded iteration
+
+    return (runtimes, entropies_of_inferred_given_groundtruth, entropies_of_groundtruth_given_inferred)
 end
 
 function plot_entropies(path)
-    (entropies_of_groundtruth_given_inferred, entropies_of_inferred_given_groundtruth, indices) = calculate_entropies(path)
-
-    plot(indices, entropies_of_groundtruth_given_inferred, seriestype = :scatter, label="entropy of groundtruth given inferred", legend=:bottomleft)
-    plot!(indices, entropies_of_inferred_given_groundtruth, seriestype = :scatter, label="entropy of inferred given groundtruth")
-    plot!(title="Entropies throughout inference", xlabel="num iterations run", ylabel="entropy")
+    (times, entropies_of_inferred_given_groundtruth, entropies_of_groundtruth_given_inferred) = calculate_entropies_over_time(path)
+    plot(times, entropies_of_groundtruth_given_inferred, seriestype = :scatter, label="entropy of groundtruth given inferred", legend=:bottomleft)
+    plot!(times, entropies_of_inferred_given_groundtruth, seriestype = :scatter, label="entropy of inferred given groundtruth")
+    plot!(title="Entropies throughout inference", xlabel="time (s)", ylabel="entropy")
     gui()
 end
 
-path = realpath(joinpath(@__DIR__, "../out/runs/20200919-15_03_45"))
+path = realpath(joinpath(@__DIR__, "../out/runs/20201010-11_21_22"))
 println("read $path: ", readdir(path))
 plot_entropies(path)
 end
--- examples/entity-resolution/test/test.jl
+++ examples/entity-resolution/test/test.jl
@@ -15,25 +15,33 @@ include("../data/import_data.jl")
 include("../inference2/initial_trace.jl")
 include("../inference2/inference.jl")
 
-function save_state!(tr, i, dirname)
-    f = open(joinpath(dirname, "$i"), "w")
+function save_state!(tr, i, currenttime, dirname; filename=nothing)
+    if filename === nothing
+        filename = "iter-$i--time---" * Dates.format(currenttime, "yyyymmdd-HH_MM_SS-sss")
+    end
+    f = open(joinpath(dirname, filename), "w")
     write_state!(f, get_state(tr))
     close(f)
 end
 
-function run_inference_with_saving!(run_inference!, initial_tr, num_iters, examine!, other_args...; save_freq=1, examine_freq=1, kwargs...)
+function run_inference_with_saving!(run_inference!, initial_tr, num_iters, examine!, other_args...; groundtruth=nothing, save_freq=1, examine_freq=1, kwargs...)
     datetime = Dates.format(now(), "yyyymmdd-HH_MM_SS")
     dirname = joinpath(realpath(joinpath(@__DIR__, "../out/runs")), datetime)
     mkdir(dirname)
     println("Will save to $dirname")
 
+    if groundtruth !== nothing
+        save_state!(groundtruth, -1, Dates.now(), dirname; filename="groundtruth")
+    end
+
     function saving_examine!(i, tr)
         if i % save_freq === 0
-            save_state!(tr, i, dirname)
+            save_state!(tr, i, Dates.now(), dirname)
         end
         examine!(i, tr)
     end
 
+    save_state!(initial_tr, 0, Dates.now(), dirname)
     run_inference!(initial_tr, num_iters, saving_examine!, other_args...; examine_freq=gcd(save_freq, examine_freq), kwargs...)
 end
 
@@ -45,21 +53,83 @@ println("num entities: $(length(data.entity_strings))")
 params = ModelParams(
     num_entities=length(data.entity_strings),
     num_verbs=length(data.verb_strings),
-    num_sentences=length(data.sentences_numeric),
-    dirichlet_prior_val=10e-6,
-    beta_prior=(0.1, 10.),
-    num_relations_mean=length(data.relation_strings),
-    num_relations_var=3
+    num_sentences=length(data.sentences_numeric)*6,
+    dirichlet_prior_val=0.01,
+    beta_prior=(20, 10000),
+    num_relations_mean=length(data.relation_strings)*3*4,
+    num_relations_var=50
 )
 
-initial_tr = get_initial_trace(data.sentences_numeric, params)
+# initial_tr = get_initial_trace(data.sentences_numeric, params)
+function get_groundtruth_tr(params)
+    tr, _ = generate(generate_sentences, model_args(params), EmptyAddressTree(); check_proper_usage=false)
+    num_true_facts = length(tr[:kernel => :sampled_facts => :all_facts])
+    num_sampled_facts = length(unique(tr[:kernel => :sampled_facts => :sampled_facts]))
+    # for i=1:100
+    #     if num_true_facts - num_sampled_facts <= 4
+    #         return tr    
+    #     end
+        println("Num true: ", num_true_facts, "; num sampled: ", num_sampled_facts)
+        tr, _ = generate(generate_sentences, model_args(params), EmptyAddressTree(); check_proper_usage=false)
+        num_true_facts = length(tr[:kernel => :sampled_facts => :all_facts])
+        num_sampled_facts = length(unique(tr[:kernel => :sampled_facts => :sampled_facts]))
+        return tr
+    # end
+    # error()
+end
+
+function get_sentences_numeric_from_tr(tr)
+    return [
+        SentenceNumeric(verb=v, ent1=entpair[1], ent2=entpair[2])
+        for (v, entpair) in zip(get_retval(tr)...)
+    ]
+end
+
+groundtruth_tr = get_groundtruth_tr(params)
+println("Ground truth trace generated.")
+println("\tScore: ", get_score(groundtruth_tr))
+println("\tNum true facts: ", length(groundtruth_tr[:kernel => :sampled_facts => :all_facts]))
+println("\tNum sampled facts: ", length(unique(groundtruth_tr[:kernel => :sampled_facts => :sampled_facts])))
+println("\tNum relations: ", groundtruth_tr[:world => :num_relations => ()])
+
+initial_tr = get_initial_trace(get_sentences_numeric_from_tr(groundtruth_tr), params)
 println("Initial trace generated (score = $(get_score(initial_tr))).\nRunning inference...")
 
-examine!(i, tr) = println("Score at iteration $i : $(get_score(tr)) | num rels = $(tr[:world => :num_relations => ()])")
+function get_examine()
+    num_ones = 0
+    function examine!(i, tr)
+        # if i > 150
+            println("Score at iteration $i : $(get_score(tr)) | num rels = $(tr[:world => :num_relations => ()])")
+        # end
+        # if tr[:world => :num_relations => ()] == 1
+        #     num_ones += 1
+        #     if num_ones >= 3
+        #         error("too many visits to a state with only one relation!")
+        #     end
+        # end
+    end
+    return examine!
+end
 
 # inferred = run_tracked_splitmerge_inference!(initial_tr, 1000, examine!, log=true, log_freq=50, examine_freq=100)
-
-run_inference_with_saving!(run_tracked_splitmerge_inference!, initial_tr, 5000, examine!;
-    save_freq=50, examine_freq=50, log=true, log_freq=50
-)
+success = false
+# for _=1:10
+#     # success && break
+    # try
+            run_inference_with_saving!(run_tracked_splitmerge_inference!, initial_tr, 10000, get_examine();
+                save_freq=50, examine_freq=100, log=true, log_freq=100, groundtruth=groundtruth_tr
+            )
+            # run_tracked_splitmerge_inference!(initial_tr, 2000, get_examine(), log=true, log_freq=100, examine_freq=100)
+        println("Just finished running...")
+        success = true
+#     catch e
+#     #     if e isa ErrorException && occursin("key not found", e.msg)
+#     #         for (exc, bt) in Base.catch_stack()
+#     #             showerror(stdout, exc, bt)
+#     #             println()
+#     #         end
+#     #     end
+#         println("Breaking due to error $e")
+#     end
+# end
 end
