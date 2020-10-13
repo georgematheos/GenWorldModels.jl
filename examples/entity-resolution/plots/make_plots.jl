99dbad466a1f3e40dd24ab8e9665a875297e59eb
Marco Cusumano-Towner <imarcoam@gmail.com>
2020-10-13 15:18:34 -0400

    fix y-axis for iteration time plot

--- examples/entity-resolution/plots/make_plots.jl
+++ examples/entity-resolution/plots/make_plots.jl
@@ -27,6 +27,7 @@ savefig("entity_error_vs_time.pdf")
 
 num_sentences = [600, 1200, 2400, 3600, 4800]
 
+gwm_num_iters = 50.0
 gwm_times = [
 0.36 0.449 1.529 1.532 2.464;
 0.372 0.92 0.683 0.984 1.165;
@@ -36,8 +37,9 @@ gwm_times = [
 0.324 0.523 1.116 1.038 5.997;
 0.359 0.728 0.973 1.79 1.782;
 0.358 0.588 1.072 1.429 1.565
-]
+] / gwm_num_iters
 
+vgen_num_iters = 20.0
 vgen_times = [
 0.763	1.53	3.846	6.087	6.638;
 0.945	1.475	2.545	6.237	7.449;
@@ -45,14 +47,14 @@ vgen_times = [
 0.999	0.813	4.289	6.075	8.663;
 0.608	1.336	4.264	4.51	6.63;
 0.579	1.829	2.678	5.486	5.739
-]
+] / vgen_num_iters
 
 close("all")
 figure(figsize=(4,2))
 plot(num_sentences, median(gwm_times, dims=[1])[:], label="Ours", color="blue")
 plot(num_sentences, median(vgen_times, dims=[1])[:], label="Vanilla Gen", color="red")
 xlabel("Number of sentences")
-ylabel("Median time per MCMC iteration (sec.)")
+ylabel("Seconds per iteration")
 legend()
 tight_layout()
 savefig("iteration_time_vs_num_sentences.pdf")
