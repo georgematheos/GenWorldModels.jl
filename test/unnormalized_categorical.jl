f25a2bbea0f7b9f1db1ed48a912ee863d751a701
George Matheos <georgematheos@gmail.com>
2021-09-03 18:43:16 -0700

    unit tests for unnormalized_categorical

---
+++ test/unnormalized_categorical.jl
@@ -0,0 +1,44 @@
+@type Clust
+@oupm generate_clusters_and_samples() begin
+    @number Clust() = (return num ~ poisson_plus_1(5))
+    @property unnormalized_weight(::Clust) ~ gamma(5.0, 1.0)
+    @observation_model (static, diffs) function get_values()
+        cluster_to_itself = lazy_set_to_dict_map(identity, @objects(Clust))
+        weights = @dictmap (c => @get(unnormalized_weight[clust]) for (c, clust) in cluster_to_itself)
+
+        # Now actually do the sampling:
+        cluster_samples ~ unnormalized_categorical(@world(), 5, weights)
+        return cluster_samples
+    end
+end
+@load_generated_functions()
+
+tr, generate_weight = generate(generate_clusters_and_samples, (), choicemap(
+    @set_number(Clust(), 5),
+    (@obsmodel() => :cluster_samples => 1, Clust(1)),
+    (@obsmodel() => :cluster_samples => 2, Clust(1)),
+    (@obsmodel() => :cluster_samples => 3, Clust(1)),
+    (@obsmodel() => :cluster_samples => 4, Clust(1)),
+    (@obsmodel() => :cluster_samples => 5, Clust(2))
+));
+
+new_tr, update_weight, retdiff, discard = update(tr, (), (), choicemap(
+    (@obsmodel() => :cluster_samples => 4, Clust(2))
+))
+
+probs = [@get(tr, unnormalized_weight[Clust(i)]) for i=1:5] |> x -> x./sum(x)
+old_sampled_prob = 4*log(probs[1]) + log(probs[2])
+new_sampled_prob = 3*log(probs[1]) + 2*log(probs[2])
+@test isapprox(update_weight, new_sampled_prob - old_sampled_prob)
+
+new_tr, update_weight, retdiff, discard = update(tr, (), (), choicemap(
+    @set(unnormalized_weight[Clust(1)], 10.),
+    @set(unnormalized_weight[Clust(2)], 4.),
+))
+new_probs = vcat([10., 4.], [@get(new_tr, unnormalized_weight[Clust(i)]) for i=3:5]) |> x -> x./sum(x)
+old_sampled_prob = 4*log(probs[1]) + log(probs[2])
+new_sampled_prob = 4*log(new_probs[1]) + log(new_probs[2])
+change_from_gammas = logpdf(gamma, 10., 5., 1.) + logpdf(gamma, 4., 5., 1.) - (
+    logpdf(gamma, @get(tr, unnormalized_weight[Clust(1)]), 5., 1.) + logpdf(gamma, @get(tr, unnormalized_weight[Clust(2)]), 5., 1.)
+)
+@test isapprox(update_weight, new_sampled_prob - old_sampled_prob + change_from_gammas)
