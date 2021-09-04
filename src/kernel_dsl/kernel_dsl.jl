874bef989716303ee7b6195eeed4802a31f2dd7c
George Matheos <georgematheos@gmail.com>
2021-09-03 18:42:29 -0700

    get_world, get_world_args for TraceToken

--- src/kernel_dsl/kernel_dsl.jl
+++ src/kernel_dsl/kernel_dsl.jl
@@ -3,3 +3,4 @@ include("auto_num_change.jl")
 
 get_world(tr::UsingWorldTrace) = tr.world
 get_world(tr::GenTraceKernelDSL.TraceToken) = get_world(tr.trace)
+get_world_args(tr::GenTraceKernelDSL.TraceToken) = get_world_args(tr.trace)
